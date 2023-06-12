01c6e8b1d4a8552a212e66ed5e95bedf0812512b
AleksandrAkelev <myvansalivan@gmail.com>
2023-06-12 12:48:52 +0300

    fix: try/catch for users methods

--- controllers/users.js
+++ controllers/users.js
@@ -10,34 +10,37 @@ const secret = process.env.JWT_SECRET;
 @access Public
 */
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Пожалуйста, заполните обязательные поля" });
+    }
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: "Пожалуйста, заполните обязательные поля" });
-  }
-
-  const user = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  });
-
-  const isPasswordCorrect =
-    user && (await brypt.compare(password, user.password));
-
-  if (user && isPasswordCorrect && secret) {
-    res.status(200).json({
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
     });
-  } else {
-    return res.status(400).json({
-      message: "Неверно введен логин или пароль",
-    });
+
+    const isPasswordCorrect =
+      user && (await brypt.compare(password, user.password));
+
+    if (user && isPasswordCorrect && secret) {
+      res.status(200).json({
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+      });
+    } else {
+      return res.status(400).json({
+        message: "Неверно введен логин или пароль",
+      });
+    }
+  } catch {
+    res.status(400).json({ message: "что-то пошло не так" });
   }
 };
 
@@ -47,44 +50,50 @@ const login = async (req, res) => {
 @access Public
 */
 const register = async (req, res) => {
-  const { email, password, name } = req.body;
+  try {
+    const { email, password, name } = req.body;
 
-  if (!email || !password || !name) {
-    return res.status(400).json({ message: "Заполните обязательные поля" });
-  }
-  // Проверка на наличие пользователя в базе данных
-  const registerUser = await prisma.user.findFirst({
-    where: {
-      email,
-    },
-  });
-
-  if (registerUser) {
-    return res
-      .status(400)
-      .json({ message: "Пользователь с таким email уже существует" });
-  }
-
-  const salt = await brypt.genSalt(10);
-  const hashedPassord = await brypt.hash(password, salt);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      name,
-      password: hashedPassord,
-    },
-  });
-
-  if (user && secret) {
-    res.status(201).json({
-      id: user.id,
-      email: user.email,
-      name,
-      token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+    if (!email || !password || !name) {
+      return res.status(400).json({ message: "Заполните обязательные поля" });
+    }
+    // Проверка на наличие пользователя в базе данных
+    const registerUser = await prisma.user.findFirst({
+      where: {
+        email,
+      },
     });
-  } else {
-    return res.status(400).json({ message: "Не удалось создать пользователя" });
+
+    if (registerUser) {
+      return res
+        .status(400)
+        .json({ message: "Пользователь с таким email уже существует" });
+    }
+
+    const salt = await brypt.genSalt(10);
+    const hashedPassord = await brypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassord,
+      },
+    });
+
+    if (user && secret) {
+      res.status(201).json({
+        id: user.id,
+        email: user.email,
+        name,
+        token: jwt.sign({ id: user.id }, secret, { expiresIn: "30d" }),
+      });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Не удалось создать пользователя" });
+    }
+  } catch {
+    res.status(400).json({ message: "что-то пошло не так" });
   }
 };
 
